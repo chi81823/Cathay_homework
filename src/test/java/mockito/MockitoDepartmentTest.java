dc2b28c191ba34f3cf92910f3c55fd8a8b7d1e92
DarrenChi <chi81823@gmail.com>
2019-04-04 18:03:10 +0800

    add Mockito Test

--- src/test/java/mockito/MockitoDepartmentTest.java
+++ src/test/java/mockito/MockitoDepartmentTest.java
@@ -1,8 +1,65 @@
 package mockito;
 
+import com.example.entity.Department;
+import com.example.repository.DepartmentRepository;
+import com.example.service.DepartmentService;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.ArgumentMatchers.any;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MockitoDepartmentTest {
+
+    @InjectMocks
+    private DepartmentService service;
+
+    @Mock
+    private DepartmentRepository repository;
+
+    @Test
+    public void saveDepartmentTest() {
+        Department department = Department.Builder.create()
+                                                  .number("0001")
+                                                  .name("IT")
+                                                  .build();
+
+        Mockito.when(repository.save(any(Department.class))).thenReturn(department);
+
+        Department returned = service.save(department);
+
+        Assert.assertEquals(department, returned);
+    }
+
+    @Test
+    public void updateDepartmentTest() {
+        Department department = Department.Builder.create()
+                                                  .id(1L)
+                                                  .number("0001")
+                                                  .name("IT")
+                                                  .build();
+
+        Department update = Department.Builder.create()
+                                              .id(1L)
+                                              .number("0002")
+                                              .name("HR")
+                                              .build();
+
+        Mockito.when(repository.findOne(department.getId())).thenReturn(department);
+        Mockito.when(service.update(department.getId(), update)).thenReturn(update);
+
+        Assert.assertNotEquals(department, update);
+    }
+
+    @Test
+    public void deleteDepartmentTest() {
+        Mockito.when(repository.findOne(1L)).thenReturn(null);
+
+        service.delete(1L);
+    }
 }
--- src/test/java/mockito/MockitoEmployeeTest.java
+++ src/test/java/mockito/MockitoEmployeeTest.java
@@ -1,10 +1,12 @@
 package mockito;
 
 import com.example.entity.Employee;
+import com.example.repository.EmployeeRepository;
 import com.example.service.EmployeeService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -12,20 +14,78 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+
 @RunWith(MockitoJUnitRunner.class)
 public class MockitoEmployeeTest {
 
-    @Mock
+    @InjectMocks
     private EmployeeService service;
 
-    @Test
-    public void asd() {
+    @Mock
+    private EmployeeRepository repository;
 
+    @Test
+    public void saveEmployeeTest() {
+        Employee employee = Employee.Builder.create()
+                                            .number("0001")
+                                            .name("Darren")
+                                            .departmentId(2L)
+                                            .gender("M")
+                                            .telephone("0912345678")
+                                            .address("新北市")
+                                            .age(28)
+                                            .build();
+
+        Mockito.when(repository.save(any(Employee.class))).thenReturn(employee);
+
+        Employee returned = service.save(employee);
+
+        Assert.assertEquals(employee, returned);
+    }
+
+    @Test
+    public void updateEmployeeTest() {
+        Employee employee = Employee.Builder.create()
+                                            .id(1L)
+                                            .number("0001")
+                                            .name("Darren")
+                                            .departmentId(2L)
+                                            .gender("M")
+                                            .telephone("0912345678")
+                                            .address("新北市")
+                                            .age(28)
+                                            .build();
+
+        Employee update = Employee.Builder.create()
+                                          .number("0001")
+                                          .name("Lisa")
+                                          .departmentId(2L)
+                                          .gender("F")
+                                          .telephone("0912345678")
+                                          .address("新北市")
+                                          .age(28)
+                                          .build();
+
+        Mockito.when(repository.findOne(employee.getId())).thenReturn(employee);
+        Mockito.when(service.update(employee.getId(), update)).thenReturn(update);
+
+        Assert.assertNotEquals(employee, update);
+    }
+
+    @Test
+    public void deleteEmployeeTest() {
+        Mockito.when(repository.findOne(1L)).thenReturn(null);
+
+        service.delete(1L);
+    }
+
+    @Test
+    public void searchEmployeeTest() {
         List<Employee> employeeList = new ArrayList<>();
 
-        Mockito.when(service.getEmployeeAll()).thenReturn(employeeList);
+        Mockito.when(service.search("01", "Dar", 28, 2L, null, null)).thenReturn(employeeList);
 
         Assert.assertNotNull(employeeList);
-
     }
 }
